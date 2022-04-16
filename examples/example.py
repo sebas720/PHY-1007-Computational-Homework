237965a4697ce063489d265efc7bd29155e94301
sebas720 <94081811+sebas720@users.noreply.github.com>
2022-04-16 16:41:38 -0400

    Création circuit a)
    Le circuit risque de ne pas être totalement au point (question en suspend).

--- examples/example.py
+++ examples/example.py
@@ -6,11 +6,16 @@ from src import Circuit, Current, Wire, World
 if __name__ == "__main__":
     world = World(shape=(51, 51))
 
-    wires = [
-        # TODO : Add wires here
+    WIRES = [Wire(start=(25, 0), stop=(0, 0), current=Current(x=0, y=1), voltage=4.5),
+        Wire(start=(0, 0), stop=(0, 50), current=Current(x=1, y=0), voltage=4.5),
+        Wire(start=(0, 50), stop=(25, 50), current=Current(x=0, y=-1), voltage=4.5),
+        Wire(start=(25, 50), stop=(50, 50), current=Current(x=0, y=-1), voltage=-4.5),
+        Wire(start=(50, 50), stop=(50, 0), current=Current(x=-1, y=0), voltage=-4.5),
+        Wire(start=(50, 0), stop=(25, 0), current=Current(x=0, y=1), voltage=-4.5),
     ]
+        # TODO : Add wires here
 
-    circuit = Circuit(wires=wires)
+    circuit = Circuit(wires=WIRES)
 
     world.place(circuit)
 
